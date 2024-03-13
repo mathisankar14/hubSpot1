d6edd45ce5e84dc8dd90c783e451f19bdf59c96b
mathisankar14 <116430862+mathisankar14@users.noreply.github.com>
2024-03-13 21:31:12 +0530

    Add files via upload

---
+++ HubSpot.js
@@ -0,0 +1,38 @@
+// document.addEventListener("keyup",(event) => {
+//     switch (event.key) {
+//         case "ArrowUp":
+//             alert("Up Arrow pressed");
+//             break;
+//         case "ArrowDown":
+//             alert("Down Arrow pressed");
+//             break;
+//         case "ArrowRight":
+//             alert("Right Arrow pressed");
+//             break;
+//         case "ArrowLeft":
+//             alert("Left Arrow pressed");
+//             break;
+
+//         default:
+//             alert("Nothing")
+//             break;
+//     }
+// });
+
+// document.addEventListener("mousedown",(event) => {
+//     switch (event.key) {
+//         case "Left":
+//             alert("Up Arrow pressed");
+//             break;
+//         case "mid":
+//             alert("Down Arrow pressed");
+//             break;
+//         case "Right":
+//             alert("Right Arrow pressed");
+//             break;
+
+//         default:
+//             alert("Nothing")
+//             break;
+//     }
+// });
